9ff0cea6b5e68e849acacbbdcf0dd9c85749c9b7
Andrea103 <andreasierra103@gmail.com>
2023-08-18 14:24:50 +0200

    add footer operating with copyride

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,20 @@
-<script >
+<template>
+  <div>
+    <header><h1>Tarea diarias</h1></header>
+    <main></main>
+    <FooterToDo> </FooterToDo>
+  </div>
+</template>
+
+<script>
+import FooterToDo from './components/FooterToDo.vue'
+
 export default {
   name: 'App',
   components: {
-
+    FooterToDo
   }
 }
 </script>
 
-<template>
-  <h1>Tarea diarias</h1>
-
-</template>
-
-<style >
-
-</style>
+<style></style>
