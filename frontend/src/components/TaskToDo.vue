6cf0f9e9b7e65bb2103064a2b7f282e594e1fa0e
Angela-coello <angeladarko32@gmail.com>
2023-08-28 09:28:42 +0200

    task

--- frontend/src/components/TaskToDo.vue
+++ frontend/src/components/TaskToDo.vue
@@ -1,78 +1,71 @@
+<!-- 
+
 <template>
-    <div class="container ">
+    <div class="container">
         <div class="row">
-            <div class="col-lg-8 offset-lg-2 mt-5 ">
-            <input type="text" class="form-control form-control-lg" placeholder="Ingresar Tarea.." v-model="nombreTarea" v-on:keyup.enter="agregarTarea()"/>
-            <br>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-8 offset-lg-2">
-            <div class="card p-2" v-if="tareas.length === 0">
-                <h6>No hay tareas para mostrar</h6>
+            <div class="col-lg-8 offset-lg-2 mt-5">
+                <input type="text" class="form-control form-control-lg" placeholder="Enter Task.." v-model="taskName" v-on:keyup.enter="addTask()" />
+                <br>
             </div>
-            <buton class="btn btn-secondary mt-3" @click="reiniciarLista">Reiniciar Lista</buton>
-            <ul class="list-group" v-for="(tarea, index) in tareas" v-bind:key="index">
-                <li class="list-group-item d-flex justify-content-between">
-                    <span class="cursor" @click="actualizarTarea(tarea)">
-                        <i class="fa fa-circle"  v-if="!tarea.estado"></i>
-                        <i class="fas fa-check-circle" v-else  :class="{'text-success': tarea.estado}"></i>
-                    </span>
-                    <h5>
-                        {{ tarea.nombre }} - <span :class="{ 'text-danger': !tarea.estado, 'text-success': tarea.estado}"> {{ tarea.estado ? 'Completada': 'Pendiente'}}</span>
-                    </h5>
-
-                    <input type="date" v-model="tarea.fechaLimite" class="form-control form-control-sm mr-1 small-input">
-                    <input type="time" v-model="tarea.horaLimite" class="form-control form-control-smmr-1 small-input ">
-                    <span class="cursor text-danger" @click="eliminarTarea(index)">
-                        <i class="fa fa-trash-alt"></i>   
-                    </span>
-                </li>
-            </ul>
         </div>
-    </div>
+
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="card p-2" v-if="tasks.length === 0">
+                    <h6>No tasks to display</h6>
+                </div>
+                <button class="btn btn-secondary mt-3" @click="resetList">Reset List</button>
+                <ul class="list-group" v-for="(task, index) in tasks" v-bind:key="index">
+                    <li class="list-group-item d-flex justify-content-between">
+                        <span class="cursor" @click="updateTask(task)">
+                            <i class="fa fa-circle" v-if="!task.completed"></i>
+                            <i class="fas fa-check-circle" v-else :class="{'text-success': task.completed}"></i>
+                        </span>
+                        <h5>
+                            {{ task.name }} - <span :class="{'text-danger': !task.completed, 'text-success': task.completed}">{{ task.completed ? 'Completed' : 'Pending' }}</span>
+                        </h5>
+
+                        <input type="date" v-model="task.dueDate" class="form-control form-control-sm mr-1 small-input">
+                        <input type="time" v-model="task.dueTime" class="form-control form-control-sm mr-1 small-input">
+                        <span class="cursor text-danger" @click="deleteTask(index)">
+                            <i class="fa fa-trash-alt"></i>
+                        </span>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
-
-
-
-<script setup >
+<script setup>
 import { ref } from 'vue';
 
-const tareas = ref([]);
-const nombreTarea = ref('');
+const tasks = ref([]);
+const taskName = ref('');
 
-const agregarTarea = () => {
-  const tarea = {
-    nombre: nombreTarea.value,
-    estado: false,
-    fechaLimite: null ,
-    horaLimite: null
+const addTask = () => {
+  const task = {
+    name: taskName.value,
+    completed: false,
+    dueDate: null,
+    dueTime: null
   };
-  tareas.value.push(tarea);
-  nombreTarea.value = '';
+  tasks.value.push(task);
+  taskName.value = '';
 };
 
-const eliminarTarea = (index) => {
-    tareas.value.splice(index, 1);
-
+const deleteTask = (index) => {
+    tasks.value.splice(index, 1);
 };
 
-const actualizarTarea = (tarea) => {
-    tarea.estado = !tarea.estado;
+const updateTask = (task) => {
+    task.completed = !task.completed;
 };
 
-const reiniciarLista = () => {
-    tareas.value = [];
-    nombreTarea.value = '';
+const resetList = () => {
+    tasks.value = [];
+    taskName.value = '';
 }
-
-
-
-
-
 </script>
 
 <style scoped>
@@ -84,19 +77,94 @@ input {
     width: 500px;
 }
 
-
-.cursor{
+.cursor {
     cursor: pointer;
 }
 
 .list-group-item {
-  align-items: center;
+    align-items: center;
 }
 
 .small-input {
-  font-size: 15px; 
-  padding: 0.2rem 0.4rem; 
-  width: 150px;
+    font-size: 15px;
+    padding: 0.2rem 0.4rem;
+    width: 150px;
 }
+</style> -->
 
-</style>
+<template>
+    <div class="d-flex justify-content-center align-items-center vh-100"> 
+        <div>
+            <h1 class="display-4 text-center mt-5">TO DO LIST</h1> 
+            <form @submit.prevent="agregarTarea">
+            <input v-model="nuevaTarea" class="form-control mb-2" placeholder="New task" required />
+            <button type="submit" class="btn1 btn-primary">Add Task</button>
+        </form>
+            <div class="mt-4">
+                <table class="table table-lg"> >
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>DAY</th>
+                            <th>EVENT</th>
+                            <th>ACTIONS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <a class="btn btn-warning  fa-solid fa-pen-to-square" ></a>
+                                <a class="btn btn-danger fa fa-trash-alt"></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    </template>
+    
+
+ <script setup>
+
+
+
+</script>
+  
+  
+
+
+
+  <style>
+
+  h1 {
+    color: rgb(31, 161, 161);
+  }
+ 
+ .table {
+    width: 500px;
+    
+ }
+
+ 
+ .btn1 {
+    background-color: rgb(49, 168, 168);
+ }
+  </style>
+
+
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
