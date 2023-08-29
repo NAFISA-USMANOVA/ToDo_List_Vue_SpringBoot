0c6849c8a9d630976df70deb360dc039063ad913
Angela-coello <angeladarko32@gmail.com>
2023-08-29 10:00:47 +0200

    change path router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,21 +10,19 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
+    
 
     {
-      path: '/',
-      name: 'cardTask',
-      component:() => import('../components/TaskToDo.vue')
+      
+        path: '/update',
+        name: 'form',
+        component:FormView
+      
     }
+
+   
+
   ]
-})
+});
 
 export default router
