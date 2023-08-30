047e64c4773089ac02ef80cbe065c11b247a9a4f
Andrea103 <andreasierra103@gmail.com>
2023-08-30 15:12:14 +0200

    API name change

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/probar',
+  baseURL: 'http://localhost:8080/tasks',
   Headers: {
     'content-type': 'application/json'
   }
