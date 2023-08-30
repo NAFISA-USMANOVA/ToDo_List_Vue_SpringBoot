abacbe5a73e377e31a974aa17893cada4f53967f
Andrea103 <andreasierra103@gmail.com>
2023-08-30 16:05:01 +0200

    The color changed

--- frontend/src/components/FooterToDo.vue
+++ frontend/src/components/FooterToDo.vue
@@ -29,7 +29,7 @@ export default {
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  background-color: rgba(86, 197, 191, 1);
+  background-color: rgb(123, 53, 130);
   padding: 5px;
   text-align: center;
   position: fixed;
@@ -40,9 +40,9 @@ export default {
 .developers {
   margin: 15px;
   text-decoration: none;
-  color: rgba(31, 6, 114, 1);
+  color: white;
 }
 h4 {
-  color: rgba(31, 6, 114, 1);
+  color: white;
 }
 </style>
